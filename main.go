80f950f0db6b0d44ffd22b802d26f9fdbad5bd4a
Asmi Sanjaya <ibrahimasmi0@gmail.com>
2022-08-13 05:26:15 +0000

    simple cru tasks complete

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
     "strconv"
+    "math/rand"
+    "time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -29,21 +31,21 @@ func setupRouter() *gin.Engine {
 		Id_task: 0,
         Isi:  "Fitur Menambahkan Task (Create)",
         Dikerjakan_oleh: "P1",
-        Deadline: "10 Agustus 2022",
+        Deadline: "2022-08-10",
         Status: 0,
     })
     tasks = append(tasks, Task{
 		Id_task: 1,
         Isi:  "Fitur Membaca Task (Read)",
         Dikerjakan_oleh: "P2",
-        Deadline: "11 Desember 2022",
+        Deadline: "2022-12-20",
         Status: 1,
     })
     tasks = append(tasks, Task{
 		Id_task: 2,
         Isi:  "Fitur Mengedit Task (Update)",
         Dikerjakan_oleh: "P1",
-        Deadline: "12 Oktober 2022",
+        Deadline: "2022-10-02",
         Status: 5,
     })
 	
@@ -58,16 +60,29 @@ func setupRouter() *gin.Engine {
 			
         })
 	})
+
+	r.POST("/add/save", func(c *gin.Context) {
+		rand.Seed(time.Now().UnixNano())
+		tasks = append(tasks, Task{
+			Id_task: rand.Intn(100),
+			Isi: c.PostForm("isi"),
+			Dikerjakan_oleh: c.PostForm("dikerjakan_oleh"),
+			Deadline: c.PostForm("deadline"),
+			Status: 0,
+		})
+		c.Redirect(http.StatusMovedPermanently, "/")
+    })
 	
 
 	r.GET("/edit/:id", func(c *gin.Context) {
-		var idtask int
-		idtask, err := strconv.Atoi(c.Params.ByName("id"))
+		var array_task int
+		array_task, err := strconv.Atoi(c.Params.ByName("id"))
 		
 		if err == nil {
-			tugas := tasks[idtask]
+			tugas := tasks[array_task]
 			c.HTML(http.StatusOK, "task_edit.html", gin.H{
 				"tugas_single": tugas,
+				"id" : array_task,
 			})
 		} else {
 			c.HTML(http.StatusOK, "error.html", gin.H{
@@ -75,6 +90,30 @@ func setupRouter() *gin.Engine {
 			})
 		}
 	})
+	r.POST("/edit/save/:id", func(c *gin.Context) {
+		
+		array_task, err := strconv.Atoi(c.Params.ByName("id"))
+		if err == nil {
+			var sudah int
+			rand.Seed(time.Now().UnixNano())
+			if c.PostForm("status") == "1" {
+				sudah = 1
+			} else {
+				sudah = 0
+			}
+			tasks[array_task].Id_task = rand.Intn(100)
+			tasks[array_task].Isi = c.PostForm("isi")
+			tasks[array_task].Dikerjakan_oleh = c.PostForm("dikerjakan_oleh")
+			tasks[array_task].Deadline = c.PostForm("deadline")
+			tasks[array_task].Status = sudah
+			
+			c.Redirect(http.StatusMovedPermanently, "/")
+		} else {
+			c.HTML(http.StatusOK, "error.html", gin.H{
+				
+			})
+		}
+    })
 
 	// ======================================
 	// default start
